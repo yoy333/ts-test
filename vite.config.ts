42e508fd4f01793e614431e1e32b2a4e330cc7a7
Colin Doherty <colinjdoherty@outlook.com>
2025-09-28 16:21:23 -0700

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-    // vite.config.ts
-    import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 
-    export default defineConfig({
-      build: {
-        sourcemap: true, // Set to true to generate separate sourcemap files
-        // sourcemap: 'inline', // Set to 'inline' to embed sourcemaps as data URIs
-        // sourcemap: 'hidden', // Set to 'hidden' to generate sourcemaps but suppress comments in bundled files
-      },
-    }); 
+
+  export default defineConfig({
+    build: {
+      sourcemap: true, // Set to true to generate separate sourcemap files
+      // sourcemap: 'inline', // Set to 'inline' to embed sourcemaps as data URIs
+      // sourcemap: 'hidden', // Set to 'hidden' to generate sourcemaps but suppress comments in bundled files
+    },
+    base: 'ts-test'
+  }); 
